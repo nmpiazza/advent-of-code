93ec1bfec8f47a9c9f6b95b1c9546a1dcf3b9df3
Nick Piazza <npiazza4@gmail.com>
2024-12-07 23:17:42 -0500

    2024 D2P2 This is not an elegant solution

--- 2024/day02/src/part1.rs
+++ 2024/day02/src/part1.rs
@@ -1,24 +1,25 @@
 use std::fs::read_to_string;
 
-fn read_lines(filename: &str) -> Vec<String> {
+pub fn read_lines(filename: &str) -> Vec<String> {
     read_to_string(filename)
         .unwrap()
         .lines()
         .map(String::from)
         .collect()
 }
-fn parse_line(line: String) -> Vec<i32> {
+
+pub fn parse_line(line: String) -> Vec<i32> {
     line.split_whitespace()
         .map(|x| -> i32 { x.parse::<i32>().unwrap() })
         .collect()
 }
 
-fn naive_main(filename: &str) -> i32 {
+pub fn naive_main(filename: &str) -> i32 {
     let input: Vec<String> = read_lines(filename);
     let mut count: i32 = 0;
     for line in input {
         let parsed_line: Vec<i32> = parse_line(line);
-        let safe: bool = naive_is_safe(parsed_line);
+        let safe: bool = naive_is_safe(&parsed_line);
         if safe {
             count += 1
         }
@@ -27,7 +28,7 @@ fn naive_main(filename: &str) -> i32 {
     count
 }
 
-fn naive_is_safe(input: Vec<i32>) -> bool {
+fn naive_is_safe(input: &Vec<i32>) -> bool {
     let mut differences: Vec<i32> = vec![];
 
     //let mut adjacent_difference: i32 = 0;
@@ -43,6 +44,31 @@ fn naive_is_safe(input: Vec<i32>) -> bool {
         && differences.iter().all(|&x| 1 <= x.abs() && x.abs() <= 3)
 }
 
+fn part_2(filename: &str) -> i32 {
+    let input: Vec<String> = read_lines(filename);
+    let mut count: i32 = 0;
+    for line in input {
+        let parsed_line: Vec<i32> = parse_line(line);
+        let safe: bool = naive_is_safe(&parsed_line);
+        if safe {
+            count += 1
+        }
+        // Added logic block for part 2.
+        // This is a very simple and not elegant solution
+        else {
+            for (index, value) in parsed_line.iter().enumerate() {
+                let mut tmp_vec: Vec<i32> = parsed_line.clone();
+                tmp_vec.remove(index);
+                if naive_is_safe(&tmp_vec) {
+                    count += 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    count
+}
 fn main() {
     println!(
         "test_input total safe: {}",
@@ -52,4 +78,9 @@ fn main() {
         "part_1 input total safe: {}",
         naive_main("src/part1_input.txt")
     );
+    println!(
+        "part_2 test_input total safe: {}",
+        part_2("src/part1_test_input.txt")
+    );
+    println!("part_1 input total safe: {}", part_2("src/part1_input.txt"));
 }
