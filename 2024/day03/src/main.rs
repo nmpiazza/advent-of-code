61524546f62183a3f60185e76290eae0d382f022
Nick Piazza <npiazza4@gmail.com>
2024-12-08 00:04:44 -0500

    Add capture groups for left and right

--- 2024/day03/src/main.rs
+++ 2024/day03/src/main.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
 
 const PART_1_REGEX: &str = r"mul\([0-9]+,[0-9]+\)";
-const PART_1_REGEX_WITH_CAPTURE_GROUP: &str = r"(?mul\([0-9]+,[0-9]+\))";
+const PART_1_REGEX_WITH_CAPTURE_GROUP: &str = r"mul\((?P<LEFT>[0-9]+),(?<RIGHT>[0-9]+)\)";
 const PART_1_TEST_INPUT: &str =
     "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
