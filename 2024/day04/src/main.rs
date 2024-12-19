ddc63fa216345d3df4088077e090293855e1d286
Nick Piazza <npiazza4@gmail.com>
2024-12-19 00:41:26 -0500

    2024d4p1 temp i guess

--- 2024/day04/src/main.rs
+++ 2024/day04/src/main.rs
@@ -1,3 +1,5 @@
+use std::thread::sleep_ms;
+
 const PART_1_TEST_INPUT: &str = "MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
@@ -9,45 +11,16 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX";
 
-//enum ValidChar {
-//    X,
-//    M,
-//    A,
-//    S,
-//}
-//enum ValidCharError {
-//    InvalidChar,
-//}
-//
-//impl ValidChar {
-//    fn to_char(&self) -> char {
-//        match self {
-//            ValidChar::X => return 'X',
-//            ValidChar::M => return 'M',
-//            ValidChar::A => return 'A',
-//            ValidChar::S => return 'S',
-//        }
-//    }
-//    fn from_char(c: char) -> Result<ValidChar, crate::ValidCharError> {
-//        match c {
-//            'X' => Ok(ValidChar::X),
-//            'M' => Ok(ValidChar::M),
-//            'A' => Ok(ValidChar::A),
-//            'S' => Ok(ValidChar::S),
-//            //_ => Err("Invalid character"),
-//            _ => Err(ValidCharError::InvalidChar),
-//        }
-//    }
-//}
-
+#[allow(dead_code)]
 #[derive(Debug)]
 struct Solution {
     solution: Vec<char>,
     reversible: bool,
 }
 
+#[allow(dead_code)]
 impl Solution {
-    fn matches(&self, candidate: Vec<char>) -> bool {
+    fn matches(&self, candidate: &Vec<char>) -> bool {
         if self.reversible {
             // return self.solution == candidate || self.solution.reverse().clone() == candidate;
             return self.solution.iter().eq(candidate.iter())
@@ -59,6 +32,7 @@ impl Solution {
     }
 }
 
+#[allow(dead_code)]
 struct Directions {
     north: bool,
     south: bool,
@@ -70,72 +44,44 @@ struct Directions {
     north_west: bool,
 }
 
+#[allow(dead_code)]
 impl Directions {
     fn new() -> Directions {
         Directions {
-            north: true,
-            south: true,
-            east: true,
-            west: true,
-            north_east: true,
-            south_east: true,
-            south_west: true,
-            north_west: true,
+            north: false,
+            south: false,
+            east: false,
+            west: false,
+            north_east: false,
+            south_east: false,
+            south_west: false,
+            north_west: false,
         }
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug, PartialEq)]
 struct Point {
-    X: usize,
-    Y: usize,
+    x: usize,
+    y: usize,
 }
 
-struct Match(Vec<char>);
+//struct Match(Vec<char>);
 
-/*
-fn direction(point: Point, X_size: i32, Y_size: i32) -> Vec<Point> {
-    let mut X_points: Vec<usize> = vec![];
-    let mut Y_points: Vec<usize> = vec![];
-    // TODO This doesn't account for zero in one direction
-    for x in point.X as i32..(point.X as i32 + X_size) {
-        X_points.push(x as usize);
-    }
-
-    for y in point.Y as i32..(point.Y as i32 + Y_size) {
-        Y_points.push(y as usize)
-    }
-
-    X_points
-        .iter()
-        .zip(Y_points.iter())
-        .map(|(x, y)| Point { X: *x, Y: *y })
-        .collect()
-}
-*/
-
-fn direction(point: &Point, magnitude: usize, x_step: i32, y_step: i32) -> Vec<Point> {
-    let mut points: Vec<Point> = vec![];
-    for i in 1..=magnitude {
-        points.push(Point {
-            X: (point.X as i32 + i as i32 * x_step) as usize,
-            Y: (point.Y as i32 + i as i32 * y_step) as usize,
-        })
-    }
-    points
-}
-
+#[allow(dead_code)]
 #[derive(Debug)]
 enum CrosswordError {
     NoMatch,
 }
 
 #[derive(Debug)]
+#[allow(dead_code)]
 struct Crossword {
     board: Vec<Vec<char>>,
     solutions: Vec<Solution>,
 }
 
+#[allow(dead_code)]
 impl Crossword {
     fn new(input: &str) -> Crossword {
         let mut board: Vec<Vec<char>> = vec![];
@@ -149,26 +95,28 @@ impl Crossword {
         }
     }
 
-    fn check_solution(&self, position: Point, solution: &Solution) -> Vec<Match> {
+    fn check_solution(&self, position: Point, solution: &Solution) -> Vec<Vec<char>> {
+        //dbg!(&position);
+        //dbg!(&solution);
         let mut directions: Directions = Directions::new();
         // Bounds check in cardinal directions
-        if (position.X as i32) - (solution.solution.len() as i32) < 0 {
-            directions.west = false;
-        } else if (position.X as i32) + (solution.solution.len() as i32)
-            > self.board[0].len() as i32
+        if (position.x as i32) - (solution.solution.len() as i32) > 0 {
+            directions.west = true;
+        } else if (position.x as i32) + (solution.solution.len() as i32)
+            < self.board[0].len() as i32
         {
-            directions.east = false;
-        } else if (position.Y as i32) - (solution.solution.len() as i32) < 0 {
-            directions.north = false;
-        } else if (position.Y as i32) + (solution.solution.len() as i32) > self.board.len() as i32 {
-            directions.south = false;
+            directions.east = true;
+        } else if (position.y as i32) - (solution.solution.len() as i32) > 0 {
+            directions.north = true;
+        } else if (position.y as i32) + (solution.solution.len() as i32) < self.board.len() as i32 {
+            directions.south = true;
         }
 
         // AND cardinals to get the intercardinal directions
         directions.north_east = directions.north && directions.east;
         directions.south_east = directions.south && directions.east;
-        directions.north_east = directions.north && directions.east;
-        directions.south_east = directions.south && directions.east;
+        directions.north_west = directions.north && directions.west;
+        directions.south_west = directions.south && directions.west;
 
         let mut extraction_candidates: Vec<Vec<Point>> = vec![];
 
@@ -198,33 +146,41 @@ impl Crossword {
             extraction_candidates.push(direction(&position, solution.solution.len(), -1, -1))
         }
 
-        let successful_matches: Vec<Match> = vec![];
+        //dbg!(&extraction_candidates);
+        let mut successful_matches: Vec<Vec<char>> = vec![];
 
         for candidate in extraction_candidates {
             // TODO implement matches logic with extract and solution.matches()
-            todo!("Implement matches logic with extract and solution.matches()");
+            let extracted = self.extract(candidate);
+            if solution.matches(&extracted) {
+                successful_matches.push(extracted)
+            }
         }
-        /*
-        if successful_matches.is_empty() {
-            Err(CrosswordError::NoMatch)
-        } else {
-            Ok(successful_matches)
-        }
-        */
-        // NOTE Changing this to just return a Vec since I'm just extending the result anyway
+
         successful_matches
     }
 
-    fn extract(points: Vec<Point>) -> Match {
-        todo!()
+    fn extract(&self, points: Vec<Point>) -> Vec<char> {
+        //dbg!(&points);
+        let mut ret_vec: Vec<char> = vec![];
+        for point in points {
+            ret_vec.push(self.board[point.y][point.x]);
+        }
+        ret_vec
     }
 
-    fn solve(&self) -> Vec<Match> {
-        let mut matches: Vec<Match> = vec![];
+    fn solve(&self) -> Vec<Vec<char>> {
+        let mut matches: Vec<Vec<char>> = vec![];
         for x in 0..self.board[0].len() {
+            //dbg!(x);
             for y in 0..self.board.len() {
+                //dbg!(y);
                 for solution in &self.solutions {
-                    matches.extend(self.check_solution(Point { X: x, Y: y }, solution));
+                    if self.board[y][x] == solution.solution[0]
+                        || self.board[y][x] == solution.solution[solution.solution.len() - 1]
+                    {
+                        matches.extend(self.check_solution(Point { x: x, y: y }, solution));
+                    }
                 }
             }
         }
@@ -252,6 +208,196 @@ fn part1_main(input: &str) -> usize {
     //0
 }
 
+#[allow(dead_code)]
+fn print_board(board: &Vec<Vec<char>>) -> String {
+    //board.iter().for_each(|x| println!("{:#?}", x));
+    for line in board {
+        line.iter().for_each(|x| print!("{}", x));
+        println!();
+    }
+    board
+        .into_iter()
+        .map(|row| row.into_iter().collect::<String>())
+        .collect::<Vec<String>>()
+        .join("\n")
+}
+
+fn check_north(_x: usize, y: usize, len: usize, limit: usize) -> bool {
+    y as i32 - len as i32 >= limit as i32
+}
+
+fn check_south(_x: usize, y: usize, len: usize, limit: usize) -> bool {
+    y as i32 + len as i32 <= limit as i32
+}
+
+fn check_east(x: usize, _y: usize, len: usize, limit: usize) -> bool {
+    x as i32 + len as i32 <= limit as i32
+}
+
+fn check_west(x: usize, _y: usize, len: usize, limit: usize) -> bool {
+    x as i32 - len as i32 >= limit as i32
+}
+
+fn direction(point: &Point, magnitude: usize, x_step: i32, y_step: i32) -> Vec<Point> {
+    let mut points: Vec<Point> = vec![];
+    //dbg!(&points);
+    for i in 1..=magnitude {
+        //let x_val = point.X as i32 + i as i32 * x_step;
+        //let y_val = point.Y as i32 + i as i32 * y_step;
+        //dbg!(&x_val);
+        //dbg!(&y_val);
+        points.push(Point {
+            x: (point.x as i32 + i as i32 * x_step) as usize,
+            y: (point.y as i32 + i as i32 * y_step) as usize,
+        });
+        //dbg!(&points);
+    }
+    points
+}
+
+fn push_if_no_duplicates(paths: &mut Vec<Vec<Point>>, candidate: Vec<Point>) {
+    //dbg!(&paths);
+    //dbg!(&candidate);
+    let mut duplicate_found: bool = false;
+    for path in paths.iter() {
+        // Check forwards and reversed
+        if (candidate[0] == path[0] && candidate[candidate.len() - 1] == path[path.len() - 1])
+            || (candidate[0] == path[path.len() - 1] && candidate[candidate.len() - 1] == path[0])
+        {
+            duplicate_found = true;
+            break;
+        }
+    }
+    if !duplicate_found {
+        paths.push(candidate)
+    }
+}
+
+fn extract_vec_char(board: &Vec<Vec<char>>, points: &Vec<Point>) -> Vec<char> {
+    print_board(board);
+    sleep_ms(1);
+    dbg!(&points);
+    let mut ret_vec: Vec<char> = vec![];
+    for point in points {
+        ret_vec.push(board[point.y][point.x]);
+    }
+    ret_vec
+}
+
+fn simple_part_1_main(input: &str, solutions: Vec<Vec<char>>) -> usize {
+    let mut board: Vec<Vec<char>> = vec![];
+    for line in input.lines() {
+        board.push(line.chars().collect());
+    }
+    //dbg!(&board);
+    //print_board(&board);
+    //dbg!(&solutions);
+
+    let mut paths: Vec<Vec<Point>> = vec![];
+
+    for solution in solutions {
+        let magnitude: usize = solution.len();
+
+        for y in 0..board.len() {
+            for x in 0..board[0].len() {
+                let point: Point = Point { x, y };
+
+                // North
+                if check_north(x, y, solution.len(), 0) {
+                    let points = direction(&point, magnitude, 0, -1);
+                    let extracted = extract_vec_char(&board, &points);
+                    if solution.iter().eq(extracted.iter())
+                        || solution.iter().rev().eq(extracted.iter())
+                    {
+                        push_if_no_duplicates(&mut paths, points);
+                    }
+                }
+                // South
+                if check_south(x, y, solution.len(), board.len()) {
+                    let points = direction(&point, magnitude, 0, 1);
+                    let extracted = extract_vec_char(&board, &points);
+                    if solution.iter().eq(extracted.iter())
+                        || solution.iter().rev().eq(extracted.iter())
+                    {
+                        push_if_no_duplicates(&mut paths, points);
+                    }
+                }
+                // East
+                if check_east(x, y, solution.len(), board[0].len()) {
+                    let points = direction(&point, magnitude, 1, 0);
+                    let extracted = extract_vec_char(&board, &points);
+                    if solution.iter().eq(extracted.iter())
+                        || solution.iter().rev().eq(extracted.iter())
+                    {
+                        push_if_no_duplicates(&mut paths, points);
+                    }
+                }
+                // West
+                if check_west(x, y, solution.len(), 0) {
+                    let points = direction(&point, magnitude, -1, 0);
+                    let extracted = extract_vec_char(&board, &points);
+                    if solution.iter().eq(extracted.iter())
+                        || solution.iter().rev().eq(extracted.iter())
+                    {
+                        push_if_no_duplicates(&mut paths, points);
+                    }
+                }
+                // NorthEast
+                if check_north(x, y, solution.len(), 0)
+                    && check_east(x, y, solution.len(), board[0].len())
+                {
+                    let points = direction(&point, magnitude, 1, -1);
+                    let extracted = extract_vec_char(&board, &points);
+                    if solution.iter().eq(extracted.iter())
+                        || solution.iter().rev().eq(extracted.iter())
+                    {
+                        push_if_no_duplicates(&mut paths, points);
+                    }
+                }
+                // SouthEast
+                if check_south(x, y, solution.len(), board.len())
+                    && check_east(x, y, solution.len(), board[0].len())
+                {
+                    let points = direction(&point, magnitude, 1, 1);
+                    let extracted = extract_vec_char(&board, &points);
+                    if solution.iter().eq(extracted.iter())
+                        || solution.iter().rev().eq(extracted.iter())
+                    {
+                        push_if_no_duplicates(&mut paths, points);
+                    }
+                }
+                // SouthWest
+                if check_south(x, y, solution.len(), board.len())
+                    && check_west(x, y, solution.len(), 0)
+                {
+                    let points = direction(&point, magnitude, -1, 1);
+                    let extracted = extract_vec_char(&board, &points);
+                    if solution.iter().eq(extracted.iter())
+                        || solution.iter().rev().eq(extracted.iter())
+                    {
+                        push_if_no_duplicates(&mut paths, points);
+                    }
+                }
+                // NorthWest
+                if check_north(x, y, solution.len(), 0) && check_west(x, y, solution.len(), 0) {
+                    let points = direction(&point, magnitude, -1, -1);
+                    let extracted = extract_vec_char(&board, &points);
+                    if solution.iter().eq(extracted.iter())
+                        || solution.iter().rev().eq(extracted.iter())
+                    {
+                        push_if_no_duplicates(&mut paths, points);
+                    }
+                }
+            }
+        }
+    }
+
+    paths.len()
+}
+
 fn main() {
-    println!("Part 1 Test: {}", part1_main(PART_1_TEST_INPUT));
+    println!(
+        "Part 1 Test: {}",
+        simple_part_1_main(PART_1_TEST_INPUT, vec!["XMAS".chars().collect()])
+    );
 }
