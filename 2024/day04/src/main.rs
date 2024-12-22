af37d12c5ba6d2038b6dba742015084386e92d96
Nick Piazza <npiazza4@gmail.com>
2024-12-22 00:17:02 -0500

    This should not have kicked my ass

--- 2024/day04/src/main.rs
+++ 2024/day04/src/main.rs
@@ -1,4 +1,6 @@
-use std::thread::sleep_ms;
+use core::fmt;
+use std::error::Error;
+use std::fs::read_to_string;
 
 const PART_1_TEST_INPUT: &str = "MMMSXXMASM
 MSAMXMSMSA
@@ -62,10 +64,10 @@ impl Directions {
 
 #[derive(Clone, Debug, PartialEq)]
 struct Point {
-    x: usize,
-    y: usize,
+    x: i32,
+    y: i32,
 }
-
+/*
 //struct Match(Vec<char>);
 
 #[allow(dead_code)]
@@ -207,7 +209,8 @@ fn part1_main(input: &str) -> usize {
     // Count solutions
     //0
 }
-
+*/
+/*
 #[allow(dead_code)]
 fn print_board(board: &Vec<Vec<char>>) -> String {
     //board.iter().for_each(|x| println!("{:#?}", x));
@@ -394,10 +397,187 @@ fn simple_part_1_main(input: &str, solutions: Vec<Vec<char>>) -> usize {
 
     paths.len()
 }
+*/
 
+struct BoardOffset {
+    one: Point,
+    two: Point,
+    three: Point,
+}
+
+const NORTH_OFFSET: BoardOffset = BoardOffset {
+    one: { Point { x: 0, y: -1 } },
+    two: { Point { x: 0, y: -2 } },
+    three: { Point { x: 0, y: -3 } },
+};
+const SOUTH_OFFSET: BoardOffset = BoardOffset {
+    one: { Point { x: 0, y: 1 } },
+    two: { Point { x: 0, y: 2 } },
+    three: { Point { x: 0, y: 3 } },
+};
+const EAST_OFFSET: BoardOffset = BoardOffset {
+    one: { Point { x: 1, y: 0 } },
+    two: { Point { x: 2, y: 0 } },
+    three: { Point { x: 3, y: 0 } },
+};
+const WEST_OFFSET: BoardOffset = BoardOffset {
+    one: { Point { x: -1, y: 0 } },
+    two: { Point { x: -2, y: 0 } },
+    three: { Point { x: -3, y: 0 } },
+};
+
+const NORTHEAST_OFFSET: BoardOffset = BoardOffset {
+    one: { Point { x: 1, y: -1 } },
+    two: { Point { x: 2, y: -2 } },
+    three: { Point { x: 3, y: -3 } },
+};
+
+const SOUTHEAST_OFFSET: BoardOffset = BoardOffset {
+    one: { Point { x: 1, y: 1 } },
+    two: { Point { x: 2, y: 2 } },
+    three: { Point { x: 3, y: 3 } },
+};
+
+const SOUTHWEST_OFFSET: BoardOffset = BoardOffset {
+    one: { Point { x: -1, y: 1 } },
+    two: { Point { x: -2, y: 2 } },
+    three: { Point { x: -3, y: 3 } },
+};
+
+const NORTHWEST_OFFSET: BoardOffset = BoardOffset {
+    one: { Point { x: -1, y: -1 } },
+    two: { Point { x: -2, y: -2 } },
+    three: { Point { x: -3, y: -3 } },
+};
+
+//const XMAS: [char; 4] = ['X', 'M', 'A', 'S'];
+
+#[derive(Debug)]
+enum DirCheckError {
+    MatchError,
+}
+
+impl std::error::Error for DirCheckError {}
+impl fmt::Display for DirCheckError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DirCheckError::MatchError => write!(f, "No match"),
+        }
+    }
+}
+
+fn dir_check(
+    board: &Vec<Vec<char>>,
+    x: i32,
+    y: i32,
+    offset: BoardOffset,
+) -> Result<Vec<Point>, DirCheckError> {
+    let mut path: Vec<Point> = vec![];
+    if y + offset.three.y < 0
+        || x + offset.three.x < 0
+        || y + offset.three.y >= board.len() as i32
+        || x + offset.three.x >= board[0].len() as i32
+    {
+        return Err(DirCheckError::MatchError);
+    }
+    //dbg!(board[(y + offset.one.y) as usize][(x + offset.one.x) as usize]);
+
+    //dbg!(board[y as usize][x as usize]);
+    //println!("board[{}][{}]: {}", y, x, board[y as usize][x as usize]);
+    if board[(y + offset.one.y) as usize][(x + offset.one.x) as usize] == 'M'
+        && board[(y + offset.two.y) as usize][(x + offset.two.x) as usize] == 'A'
+        && board[(y + offset.three.y) as usize][(x + offset.three.x) as usize] == 'S'
+    {
+        //dbg!("here");
+        path.push(Point { x: x, y: y });
+        path.push(Point {
+            x: x + offset.one.x,
+            y: y + offset.one.y,
+        });
+        path.push(Point {
+            x: x + offset.two.x,
+            y: y + offset.two.y,
+        });
+        path.push(Point {
+            x: x + offset.three.x,
+            y: y + offset.three.y,
+        });
+    }
+
+    Ok(path)
+}
+
+fn dead_simple_part_1_main(input: &str) -> usize {
+    let mut matches: Vec<Vec<Point>> = vec![];
+    let mut board: Vec<Vec<char>> = vec![];
+
+    for line in input.lines() {
+        board.push(line.chars().collect());
+    }
+    //println!("board len: {}", board.len());
+    //println!("board[0] len: {}", board[0].len());
+
+    for y in 0..board.len() {
+        for x in 0..board[0].len() {
+            /*
+            if board[y][x] == 'X' {
+                if let x = Some(dir_check(&board, x as i32, y as i32, NORTH_OFFSET)) {
+                    matches.push(x.unwrap())
+                }
+
+                //matches.push(dir_check(&board, x as i32, y as i32, NORTH_OFFSET));
+                //matches.push(dir_check(&board, x as i32, y as i32, SOUTH_OFFSET));
+                //matches.push(dir_check(&board, x as i32, y as i32, EAST_OFFSET));
+                //matches.push(dir_check(&board, x as i32, y as i32, WEST_OFFSET));
+                //matches.push(dir_check(&board, x as i32, y as i32, NORTHEAST_OFFSET));
+                //matches.push(dir_check(&board, x as i32, y as i32, SOUTHEAST_OFFSET));
+                //matches.push(dir_check(&board, x as i32, y as i32, SOUTHWEST_OFFSET));
+                //matches.push(dir_check(&board, x as i32, y as i32, NORTHWEST_OFFSET));
+            }
+            */
+
+            if board[y][x] == 'X' {
+                for offset in [
+                    NORTH_OFFSET,
+                    SOUTH_OFFSET,
+                    EAST_OFFSET,
+                    WEST_OFFSET,
+                    NORTHEAST_OFFSET,
+                    SOUTHEAST_OFFSET,
+                    SOUTHWEST_OFFSET,
+                    NORTHWEST_OFFSET,
+                ] {
+                    if let Ok(path) = dir_check(&board, x as i32, y as i32, offset) {
+                        if !path.is_empty() {
+                            matches.push(path);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //dbg!(&matches);
+    matches.retain(|x| x.len() > 0);
+    //dbg!(&matches);
+    matches.len()
+}
 fn main() {
+    /*
     println!(
         "Part 1 Test: {}",
         simple_part_1_main(PART_1_TEST_INPUT, vec!["XMAS".chars().collect()])
     );
+    */
+    /*
+    println!(
+        "Part 1 Test: {}",
+        dead_simple_part_1_main(PART_1_TEST_INPUT)
+    );
+    */
+
+    let part_1_input: &str = &read_to_string("./src/input.txt").unwrap();
+    //println!("{}", &read_to_string("./src/input.txt").unwrap());
+    //dbg!(&read_to_string("./src/input.txt").unwrap());
+    println!("Part 1: {}", dead_simple_part_1_main(part_1_input));
 }
