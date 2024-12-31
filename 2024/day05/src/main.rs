146a6af8b49e7cfb769547bd75c078c336a25a0a
Nick Piazza <npiazza4@gmail.com>
2024-12-30 23:27:18 -0500

    2024d5p1 complete; 2025d5p2 declaring bankruptcy

--- 2024/day05/src/main.rs
+++ 2024/day05/src/main.rs
@@ -34,6 +34,7 @@ const PART_1_TEST: &str = "47|53
 #[derive(Debug)]
 enum AOCError {
     Part1Error,
+    Part2Error,
 }
 
 #[allow(dead_code)]
@@ -42,6 +43,7 @@ struct Rule {
     left: usize,
     right: usize,
 }
+
 #[allow(dead_code)]
 impl Rule {
     fn new(input: &str) -> Rule {
@@ -54,7 +56,7 @@ impl Rule {
 }
 
 #[allow(dead_code)]
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct Update {
     pages: Vec<usize>,
     rules: Vec<Rule>,
@@ -63,7 +65,7 @@ struct Update {
 
 #[allow(dead_code)]
 impl Update {
-    fn new(input: &str, rules: &Vec<Rule>) -> Update {
+    fn new_from_str(input: &str, rules: &Vec<Rule>) -> Update {
         let pages: Vec<usize> = input
             .split(',')
             .map(|x| x.parse::<usize>().ok().unwrap())
@@ -77,6 +79,18 @@ impl Update {
         update.set_matching_rules(rules);
         update.is_valid();
         // { pages: input .split(',') .map(|x| x.parse::<usize>().ok().unwrap()) .into_iter() .collect(), rules: vec![], };
+
+        update
+    }
+
+    fn new_from_vec(pages: Vec<usize>, rules: &Vec<Rule>) -> Update {
+        let mut update: Update = Update {
+            pages: pages.clone(),
+            rules: vec![],
+            valid: false,
+        };
+        update.set_matching_rules(rules);
+        update.is_valid();
 
         update
     }
@@ -145,14 +159,17 @@ fn parse_and_process(input: &str) -> (Vec<Rule>, Vec<Update>) {
     }
 
     let rules: Vec<Rule> = raw_rules.iter().map(|x| Rule::new(x)).collect();
-    let updates: Vec<Update> = raw_updates.iter().map(|x| Update::new(x, &rules)).collect();
+    let updates: Vec<Update> = raw_updates
+        .iter()
+        .map(|x| Update::new_from_str(x, &rules))
+        .collect();
 
     (rules, updates)
 }
 
 #[allow(dead_code)]
 fn part_1_main(input: &str) -> Result<usize, AOCError> {
-    let (rules, mut updates) = parse_and_process(input);
+    let (_rules, mut updates) = parse_and_process(input);
 
     let mut sum: usize = 0;
     for update in updates.iter_mut() {
@@ -166,10 +183,71 @@ fn part_1_main(input: &str) -> Result<usize, AOCError> {
     Ok(sum)
 }
 
+fn eval_rule_lt(l: usize, r: usize, rule: &Rule) -> Result<bool, AOCError> {
+    if !((l == rule.left && r == rule.right) || (l == rule.right && l == rule.left)) {
+        return Err(AOCError::Part2Error);
+    }
+
+    Ok(l == rule.left && r == rule.right)
+}
+fn fix_bubble_sort(orig: Update) -> Update {
+    if orig.valid {
+        return orig;
+    }
+
+    // TODO remove dummy
+    let mut new: Update = orig.clone();
+
+    while !new.valid {
+        let mut eval_rule = Rule { left: 0, right: 0 };
+
+        for i in 1..new.pages.len() {
+            for rule in &new.rules {
+                if (rule.left == new.pages[i] && rule.right == new.pages[i - 1])
+                    || (rule.left == new.pages[i - 1] && rule.right == new.pages[i])
+                {
+                    eval_rule = rule.clone();
+                }
+            }
+
+            dbg!(&eval_rule);
+            if !eval_rule_lt(new.pages[i - 1], new.pages[i], &eval_rule).expect("rule evaluation") {
+                new.pages.swap(i - 1, i);
+                new.is_valid();
+            }
+        }
+    }
+
+    new
+}
+fn part_2_main(input: &str) -> Result<usize, AOCError> {
+    let mut sum: usize = 0;
+    let (_rules, mut updates) = parse_and_process(input);
+
+    let mut modified_updates: Vec<(Update, Update)> = vec![];
+
+    for update in updates {
+        if update.valid {
+            continue;
+        }
+        let fixed_update: Update = fix_bubble_sort(update.clone());
+        modified_updates.push((update.clone(), fixed_update));
+    }
+    // if invalid
+    // try permutations
+    // short circuit if valid
+    // sum middle
+    Ok(sum)
+}
 fn main() {
     println!("Part 1 Test: {}", part_1_main(PART_1_TEST).unwrap());
     println!(
         "Part 1: {}",
         part_1_main(&read_to_string(PART_1_PATH).unwrap()).unwrap()
+    );
+    println!("Part 2 Test: {}", part_2_main(PART_1_TEST).unwrap());
+    println!(
+        "Part 2: {}",
+        part_2_main(&read_to_string(PART_1_PATH).unwrap()).unwrap()
     );
 }
